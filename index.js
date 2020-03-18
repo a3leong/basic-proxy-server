40b282e1649c3855ac7a302736889b8495fd88ca
Aaron-Leong <Aaron.Leong@ibm.com>
2020-03-18 11:29:40 -0700

    templates and catch all routes, did not forward data yet

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const app = express();
 const port = 4000;
 const axios = require('axios');
-const AUTH_TOKEN = '<PUT YOUR AUTH TOKEN HERE';
-axios.defaults.baseURL = '<PUT YOUR BASE_URL HERE';
-axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+const config = require('./config.js');
+axios.defaults.baseURL = config.BASE_URL;
+axios.defaults.headers.common['Authorization'] = config.AUTH_TOKEN;
 
-app.get('/*', (req, res) => {
-  console.log(req.url);
+app.all('/*', (req, res) => {
   const config = {
     url: req.url,
-    method: 'get',
+    method: req.method,
   };
   axios(config).then(proxyCallRes => {
     res.send(proxyCallRes.data);
@@ -19,9 +18,5 @@ app.get('/*', (req, res) => {
     res.status(500).send({ error: 'Unexpected error', msg: err });
   });
 });
-
-// app.get('/test', (req, res) => {
-
-// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
